bd0768c61e79ec53ff6d9f388171a78dea2fa1c3
SalahAltamimi <salahabufarha1996@outlook.com>
2024-06-22 00:23:48 +0300

    http

--- server/utils/database.js
+++ server/utils/database.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv').config({ path: './config.env' });
 exports.connect = async () => {
   try {
-    await mongoose.connect(process.env.DBS);
+    await mongoose.connect(process.env.DBS, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
     console.log('DB successfully running');
   } catch (err) {
     console.log(err.message);
