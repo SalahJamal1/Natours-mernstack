880272fe93e13e969be588d5cf8ad530937cf73d
SalahAltamimi <salahabufarha1996@outlook.com>
2024-06-22 15:47:23 +0300

    final

--- client/src/service/Api.js
+++ client/src/service/Api.js
@@ -14,51 +14,42 @@ export async function getTourslug(slug) {
 }
 export async function Login(data) {
   const res = await api.post("users/login", data);
-  console.log(res);
   return res;
 }
 export async function Logout() {
   const res = await api.get(`users/logout`);
-  console.log(res);
   return res;
 }
 export async function Signup(data) {
   const res = await api.post(`users/signup`, data);
-  console.log(res);
   return res;
 }
 export async function Getuser() {
   const res = await api.get(`users/me`);
 
-  console.log(res);
   return res;
 }
 export async function forgetPassword(data) {
   const res = await api.post(`users/forgetpassword`, data);
 
-  console.log(res);
   return res;
 }
 export async function resetpassword(id, data) {
   const res = await api.patch(`users/restpassword/${id}`, data);
-  console.log(res);
   return res;
 }
 export async function updateMe(data) {
   const res = await api.patch(`users/updateMe`, data);
 
-  console.log(res);
   return res;
 }
 export async function updateMyPassword(data) {
   const res = await api.patch(`users/updateMyPassword`, data);
 
-  console.log(res);
   return res;
 }
 export async function booking(id) {
   const res = await api.get(`booking/checkout-session/${id}`);
 
-  console.log(res);
   return res;
 }
