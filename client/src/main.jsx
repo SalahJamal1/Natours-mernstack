2bbde3eec32548402ab03ec7761e12211403ac51
SalahAltamimi <salahabufarha1996@outlook.com>
2024-06-23 00:43:39 +0300

    deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./responsive.css";
 import "leaflet/dist/leaflet.css";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
