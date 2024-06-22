20809dad3c94c6560e15235d1f2ee7440c588268
SalahAltamimi <salahabufarha1996@outlook.com>
2024-06-22 15:57:02 +0300

    final

--- server/app.js
+++ server/app.js
@@ -22,10 +22,7 @@ const app = express();
 app.use(helmet());
 
 const corsOptions = {
-  origin: [
-    'https://natours-mernstack.vercel.app/',
-    'https://natours-mernstack-b14j.vercel.app/',
-  ],
+  origin: ['https://natours-mernstack-b14j.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
 };
